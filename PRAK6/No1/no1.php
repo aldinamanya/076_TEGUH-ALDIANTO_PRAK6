aeac9cc2cf5760e065a79186c9458942afd633e0
aldinamanya <95896377+aldinamanya@users.noreply.github.com>
2022-10-21 23:10:44 +0700

    Add files via upload

---
+++ PRAK6/No1/no1.php
@@ -0,0 +1,41 @@
+<!-- TEGUH ALDIANTO -->
+<!-- 21091397076 -->
+<!-- D4 MANAJEMEN INFORMATIKA -->
+
+<?php 
+
+require_once '../no1/abstract.php';
+
+class Truck extends Vehicle {
+    public function __construct($maxLoad, $name)
+    {
+        $this->maxLoad = $maxLoad;
+        $this->name = $name;
+    }
+    
+    public function calcFuelNeeds()
+    {
+        $fuel = $this->calcFuelEfficiency();
+        $trip = $this->calcTripDistance();
+        
+        return ceil($fuel /= $trip);
+    }
+}
+
+class RiverBarge extends Vehicle {
+    public function __construct($maxLoad, $name)
+    {
+        $this->maxLoad = $maxLoad;
+        $this->name = $name;
+    }
+
+    public function calcFuelNeeds()
+    {
+        $fuel = $this->calcFuelEfficiency();
+        $trip = $this->calcTripDistance();
+
+        return ceil($fuel /= $trip);
+    }
+}
+$truck = new Truck(10000, 'Truk');
+$riverBarge = new RiverBarge(15000, 'Perahu');
---
+++ PRAK6/No4/interface.php
@@ -0,0 +1,16 @@
+<!-- TEGUH ALDIANTO -->
+<!-- 21091397076 -->
+<!-- D4 MANAJEMEN INFORMATIKA -->
+
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
